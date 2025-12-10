a293024d7a0ea579ad4c080819e4bd87ac3baf92
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-12-10 13:52:01 -0500

    Add support country query parameter on home endpoint

--- src/main/java/com/pluralsight/northwind/controllers/HomeController.java
+++ src/main/java/com/pluralsight/northwind/controllers/HomeController.java
@@ -2,13 +2,20 @@ package com.pluralsight.northwind.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home() {
-        return "Hello ELDAR";
+    public String home(@RequestParam(name = "country", required = false) String country) {
+
+        if (country == null || country.isBlank()) {
+            return "Hello World";
+        }
+
+        return "Hello " + country;
     }
 }
