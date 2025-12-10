7ad3efee471a6d0d1114eece2e6d19324168f2c9
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-12-10 13:36:38 -0500

    Initial Spring Boot REST API project

---
+++ src/main/java/com/pluralsight/northwind/StartNewApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartNewApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartNewApiProjectApplication.class, args);
+	}
+
+}
